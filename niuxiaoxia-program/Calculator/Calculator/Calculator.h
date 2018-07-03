f1e24a7407ba88e16100ba7409de8c368e19f447
niuxiaoxia-program <niuxiaoxia@126.com>
2018-07-04 07:00:38 +0800

    Message

---
+++ niuxiaoxia-program/Calculator/Calculator/Calculator.h
@@ -0,0 +1,19 @@
+#pragma once
+#pragma once
+#include "stdafx.h"
+#include <stack>
+#include <vector>
+#include <iostream>
+#include "stdlib.h"
+#include <ctime>
+#include <string>  
+using namespace std;
+
+class Calculator {
+private:
+	string op[4] = { "+", "-", "*", "/" };
+public:
+	Calculator();
+	string MakeFormula();
+	string Solve(string formula);
+};
