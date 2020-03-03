c4a26cc6d12c7fd5af2455fc83d85514ec1ea0d7
prime21 <pmxm7696@gmail.com>
2020-03-03 18:23:03 +0800

    Unit Test

---
+++ prime21/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../calc/calculator.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("11+22");
+			Assert::AreEqual(ret, (string)"11+22=33");
+		}
+	};
+}
