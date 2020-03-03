f1358e9d3fd706fa138be81b8693098ff77036d7
Qiao <1924494629@qq.com>
2020-03-03 11:18:28 +0800

    first

---
+++ JordenQiao/Calculator/Calculator/Calculator.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stack>
+#include <vector>
+#include <iostream>
+#include "stdlib.h"
+#include <ctime>
+#include <string>  
+using namespace std;
+
+class Calculator {
+private:
+	string op[4] = { "+", "-", "*", "/" };
+public:
+	Calculator();
+	string MakeFormula();
+	string Solve(string formula);
+};
